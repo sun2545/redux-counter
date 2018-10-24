cd83ca083f2e69fdd9cecba8d5f4e6b7fb05f18d
sun2545 <kwonsunhye0206@gmail.com>
2018-10-24 07:55:54 -0400

    create reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,30 @@
+import * as types from '../action/ActionTypes';
+
+const initialState ={
+    color:'black',
+    number:0
+};
+
+function counter(state =initialState, action)
+{
+    switch(action.type){
+        case types.INCREMENT :
+        return {
+            ...state,
+            number : state.number +1
+        };
+        case types.DECREMENT :
+        return {
+            ...state,
+            number : state.number -1
+        };
+        case types.SET_COLOR :
+        return {
+            color : action.color
+        };
+        default :
+        return state;
+    }
+};
+
+export default counter;
