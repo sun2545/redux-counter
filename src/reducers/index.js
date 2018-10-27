8633e40700f15b3e34df5f5bc53e639d5b7d7ff1
sun2545 <kwonsunhye0206@gmail.com>
2018-10-26 23:11:05 -0400

    Modify for making reducers separately

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,30 +1,11 @@
-import * as types from '../action/ActionTypes';
+import number from './number';
+import color from './color';
 
-const initialState ={
-    color:'black',
-    number:0
-};
+import {combineReducers} from 'redux';
 
-function counter(state =initialState, action)
-{
-    switch(action.type){
-        case types.INCREMENT :
-        return {
-            ...state,
-            number : state.number +1
-        };
-        case types.DECREMENT :
-        return {
-            ...state,
-            number : state.number -1
-        };
-        case types.SET_COLOR :
-        return {
-            color : action.color
-        };
-        default :
-        return state;
-    }
-};
+const reducers = combineReducers({
+    numberData:number,
+    colorData:color
+});
 
-export default counter;
+export default reducers;
